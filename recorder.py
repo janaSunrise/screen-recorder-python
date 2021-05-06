54ee2a157e47f3f07cce001fc6f9a0b4cab67af3
janaSunrise <warriordefenderz@gmail.com>
2021-05-06 12:51:12 +0530

    Fixed bugs in the CV2 method, and Codec.

--- recorder.py
+++ recorder.py
@@ -2,14 +2,18 @@ import cv2
 import numpy as np
 from PIL import ImageGrab
 
-four_cc = cv2.VideoWriter_fourcc(*"X264")
-size = (ImageGrab.grab()).size()
+four_cc = cv2.VideoWriter_fourcc(*"MP4V")
+size = (ImageGrab.grab()).size
 
-output_file = cv2.VideoWriter("recording.mp4", four_cc, 5.0, size)
+output_file = cv2.VideoWriter("recording.mp4", four_cc, 20.0, size)
 
 while True:
     # Grab the image, and add it to array
-    image = np.array(ImageGrab.grab())
+    image = ImageGrab.grab(bbox=(0, 0, size[0], size[1]))
+    image = np.array(image)
+
+    # Make the Numpy Image BGR to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # If you need the preview, use cv2, else comment the line below.
     cv2.imshow("Screen recording preview", image)
